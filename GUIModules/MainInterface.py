3ba51a9be5a29b82643ed8f7dff6b1a1d8dd8784
julianstar88 <milites-julian@web.de>
2020-05-26 18:19:37 +0200

    progress in MainInterface

--- GUIModules/MainInterface.py
+++ GUIModules/MainInterface.py
@@ -8,7 +8,6 @@ Created on Thu May 14 14:58:37 2020
 import sys
 import MainModules.GraphicalEvaluator as ge
 import GuiModules.CustomGuiComponents as cc
-import UtilityModules.GraphicUtilityModules as gum
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -22,11 +21,16 @@ class MainWindow(QtWidgets.QMainWindow):
 
         labels = ["Name:", "Start:", "End:", "Trainingmode:"]
         values = ["None", "None", "None", "None"]
-        self.panel1 = Panel(labels, values, fontSize = 8, split = [1,5])
+        self.panel1 = GridPanel(labels, values, fontSize = 8, split = [1,5])
 
-        labels = ["None:"]
-        values = ["None"]
-        self.panel2 = Panel(labels, values, fontSize = 8, split = [1,15])
+        labels = ["None:", "None:"]
+        values = ["None", "None"]
+        self.panel2 = DynamicLinePanel(
+                labels, values,
+                fontSize = 8,
+                split = [1,5],
+                lineMinHeight = 50
+            )
 
         self.routineTab = RoutineTab()
         self.evaluatorTab = EvaluatorTab()
@@ -42,12 +46,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.buttonLayout.addWidget(self.deleteNoteButton)
 
         self.mainLayout.addWidget(self.panel1, 0, 0)
-        self.mainLayout.addLayout(self.buttonLayout, 1, 0)
-        self.mainLayout.addWidget(self.panel2, 2, 0)
+        self.mainLayout.addWidget(self.panel2, 1, 0)
+        self.mainLayout.addLayout(self.buttonLayout, 2, 0)
         self.mainLayout.addWidget(self.tabWidget, 0, 1, 3, 1)
-        self.mainLayout.setRowStretch(2, 2)
+        self.mainLayout.setRowStretch(1, 2)
         self.mainLayout.setColumnStretch(0, 1)
         self.mainLayout.setColumnStretch(1, 2)
+        self.mainLayout.setSpacing(20)
 
         self.__connectButtons()
 
@@ -55,12 +60,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __connectButtons(self):
         self.addNoteButton.clicked.connect(self.addNote)
+        self.deleteNoteButton.clicked.connect(self.deleteNote)
 
     def addNote(self, event):
         labels = self.panel2.labels()
         values = self.panel2.values()
         labels.append("None:")
-        values.append("123456789-----"*20)
+        values.append("0123456789----- "*20)
         self.panel2.setLabels(labels)
         self.panel2.setValues(values)
         self.panel2.updatePanel()
@@ -68,13 +74,16 @@ class MainWindow(QtWidgets.QMainWindow):
     def deleteNote(self, event):
         labels = self.panel2.labels()
         values = self.panel2.values()
-        labels.pop()
-        values.pop()
-        self.panel2.setLabels(labels)
-        self.panel2.setValues(values)
-        self.panel2.updatePanel()
+        if len(labels) == 0 or len(values) == 0:
+            return
+        else:
+            labels.pop()
+            values.pop()
+            self.panel2.setLabels(labels)
+            self.panel2.setValues(values)
+            self.panel2.updatePanel()
 
-class Panel(cc.CustomWidget):
+class GridPanel(cc.CustomWidget):
 
     def __init__(self, labels, values,  *args,
                  fontSize = 8,
@@ -96,7 +105,7 @@ class Panel(cc.CustomWidget):
 
         self.setStyleSheet(
                 """
-                Panel {background-color: rgba(255,255,255,100%)}
+                GridPanel {background-color: rgba(255,255,255,100%)}
                 """
             )
 
@@ -281,6 +290,261 @@ class Panel(cc.CustomWidget):
             self.layout().setRowStretch(self.layout().rowCount(), 2)
             self._widgets[i][0] = labelWidget
             self._widgets[i][1] = valueWidget
+
+    def values(self):
+        return self._values
+
+    def widgetAtPosition(self, rowIndex, columnIndex):
+        if rowIndex > len(self.labels()) or rowIndex < 0:
+            raise IndexError(
+                    "input <{input_name}> for 'rowIndex' is out of range".format(
+                            input_name = str(rowIndex)
+                        )
+                )
+        if columnIndex > 2 or columnIndex < 0:
+            raise IndexError(
+                    "input <{input_name}> for 'columnIndex' is out of range".format(
+                            input_name = str(columnIndex)
+                        )
+                )
+        if len(self.labels()) != len(self.values()):
+            raise RuntimeError(
+                    "the attribrutes 'labels' and 'values' must have the same length"
+                )
+        return self._widgets[rowIndex][columnIndex]
+
+class DynamicLinePanel(cc.CustomWidget):
+
+    def __init__(self, labels, values, *args,
+                 fontSize = 8,
+                 split = [1, 1],
+                 lineMinHeight = 50):
+
+        super().__init__(*args)
+        self._labels = None
+        self._values = None
+        self._fontSize = None
+        self._widgets = None
+        self._split = None
+        self._lineMinHeight = None
+
+        self._mainLayout = cc.CustomBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
+        self._mainLayout.setContentsMargins(0,0,0,0)
+        self._mainWidget = None
+        self._scrollArea = None
+
+        self.setLabels(labels)
+        self.setValues(values)
+        self.setFontSize(fontSize)
+        self.setSplit(split)
+        self.setLineMinHeight(lineMinHeight)
+        self.setLayout(self._mainLayout)
+
+        self.__createPanel()
+
+    def __initiateWidgets(self):
+        if len(self.values()) != len(self.labels()):
+            raise RuntimeError(
+                    "labels have to be the same length as values"
+                )
+
+        self._widgets = []
+        for i in range(len(self.labels())):
+            insert = [None, None]
+            self._widgets.insert(i, insert)
+
+    def __createPanel(self):
+        if len(self.labels()) != len(self.values()):
+            raise RuntimeError(
+                    "labels must have the same length as values"
+                )
+        self.__initiateWidgets()
+
+        self._mainWidget = cc.CustomWidget()
+        self._scrollArea = cc.CustomScrollArea(self._mainWidget)
+        self._scrollArea.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self._mainLayout.addWidget(self._scrollArea)
+
+        layout = cc.CustomBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
+        layout.setContentsMargins(0,0,0,0)
+
+        for i,_ in enumerate(self.labels()):
+            subLayout = cc.CustomBoxLayout(QtWidgets.QBoxLayout.LeftToRight)
+            subLayout.setSpacing(0)
+            label = self.labels()[i]
+            value = self.values()[i]
+
+            labelString = label
+            labelFont = QtGui.QFont()
+            labelFont.setBold(True)
+            labelFont.setPointSize(self.fontSize())
+            labelWidget = QtWidgets.QLabel(labelString)
+            labelWidget.setTextFormat(QtCore.Qt.RichText)
+            labelWidget.setMargin(11)
+            labelWidget.setFont(labelFont)
+            labelWidget.setMinimumHeight(self.lineMinHeight())
+            labelWidget.setStyleSheet(
+                """
+                QLabel {background-color: rgba(255,255,255,100%)}
+                """
+                )
+
+            valueString = value
+            valueFont = QtGui.QFont()
+            valueFont.setPointSize(self.fontSize())
+            valueWidget = QtWidgets.QLabel(valueString)
+            valueWidget.setWordWrap(True)
+            valueWidget.setTextFormat(QtCore.Qt.RichText)
+            valueWidget.setMargin(11)
+            valueWidget.setMinimumHeight(self.lineMinHeight())
+            valueWidget.setStyleSheet(
+                """
+                QLabel {background-color: rgba(255,255,255,100%)}
+                """
+                )
+
+            subLayout.addWidget(labelWidget)
+            subLayout.addWidget(valueWidget)
+
+            self._widgets[i][0] = labelWidget
+            self._widgets[i][1] = valueWidget
+
+            layout.addLayout(subLayout)
+
+        self.__calculateContentSplit()
+        layout.addStretch(2)
+        self._mainWidget.setLayout(layout)
+
+    def __calculateContentSplit(self):
+        frac1 = self.split()[0] / sum(self.split())
+        frac2 = self.split()[1] / sum(self.split())
+        width = self.width()
+        for line in self._widgets:
+            for i, widget in enumerate(line):
+                if i == 0:
+                    widget.setMaximumWidth(round(width*frac1))
+                    widget.setMinimumWidth(round(width*frac1))
+                if i == 1:
+                    widget.setMaximumWidth(round(width*frac2))
+                    widget.setMinimumWidth(round(width*frac2))
+
+    def fontSize(self):
+        return self._fontSize
+
+    def labels(self):
+        return self._labels
+
+    def lineMinHeight(self):
+        return self._lineMinHeight
+
+    def resizeEvent(self, event):
+        self.__calculateContentSplit()
+        super().resizeEvent(event)
+
+    def setFontSize(self, size):
+        if not isinstance(size, int):
+            raise TypeError(
+                    "MainInterface.Panel.setFontSize: input <{input_name}> does not match {type_name}".format(
+                            input_name = str(size),
+                            type_name = int
+                        )
+                )
+        if size < 0:
+            raise ValueError(
+                    "MainInterface.Panel.setFontSize: input <{input_name}> has to be greater than zero".format(
+                            input_name = str(size)
+                        )
+                )
+        self._fontSize = size
+
+    def setLabels(self, labels):
+        if not isinstance(labels, list):
+            raise TypeError(
+                    "Panel.setLabels: input <{input_name}> does not match {type_name}".format(
+                            input_name = str(labels),
+                            type_name = list
+                        )
+                )
+        self._labels = labels
+
+    def setLineMinHeight(self, minh):
+        if not isinstance(minh, int):
+            raise TypeError(
+                    "input <{input_name}> does not match {type_name}".format(
+                            input_name = str(minh),
+                            type_name = int
+                        )
+                )
+        if minh < 0:
+            raise ValueError(
+                    "'minh' has to be greater or equal than zero"
+                )
+        self._lineMinHeight = minh
+
+    def setSplit(self, split):
+        if not isinstance(split, list):
+            raise TypeError(
+                    "input {input_name} does not match {type_name}".format(
+                            input_name = str(split),
+                            type_name = list
+                        )
+                )
+        if len(split) != 2:
+            raise ValueError(
+                    "there have to be only two elements in 'split'"
+                )
+        if not isinstance(split[0], int) or not isinstance(split[1], int):
+            raise TypeError(
+                    "elements of 'split' does not match {type_name}".format(
+                            type_name = int
+                        )
+                )
+        self._split = split
+
+    def setValues(self, values):
+        if not isinstance(values, list):
+            raise TypeError(
+                    "Panel.setValues: input <{input_name}> does not match {type_name}".format(
+                            input_name = str(values),
+                            type_name = list
+                        )
+                )
+        self._values = values
+
+    def setWidgetAtPosition(self, widget, rowIndex, columnIndex):
+        if not isinstance(widget, QtWidgets.QWidget):
+            raise TypeError(
+                    "input <{input_name}> for 'widget' does not match {type_name}".format(
+                            input_name = str(widget),
+                            type_name = QtWidgets.QWidget
+                        )
+                )
+        if rowIndex > len(self.labels()) or rowIndex < 0:
+            raise IndexError(
+                    "input <{input_name}> for 'rowIndex' is out of range".format(
+                            input_name = str(rowIndex)
+                        )
+                )
+        if columnIndex > 2 or columnIndex < 0:
+            raise IndexError(
+                    "input <{input_name}> for 'columnIndex' is out of range".format(
+                            input_name = str(columnIndex)
+                        )
+                )
+        if len(self.labels()) != len(self.values()):
+            raise RuntimeError(
+                    "the attribrutes 'labels' and 'values' must have the same length"
+                )
+        toReplace = self.widgetAtPosition(rowIndex, columnIndex)
+        self.layout().replaceWidget(toReplace, widget)
+
+    def split(self):
+        return self._split
+
+    def updatePanel(self):
+        self._mainLayout.removeWidget(self._scrollArea)
+        self._scrollArea.deleteLater()
+        self.__createPanel()
 
     def values(self):
         return self._values
--- MainModules/test.py
+++ MainModules/test.py
@@ -4,10 +4,7 @@ Created on Thu Apr 30 18:11:28 2020
 
 @author: Julian
 """
-test = [[1,2,3], [4,5,6], [7,8,9]]
 
-for i in range(len(test)):
-    test.pop()
-
-print(test)
+t = [1,2]
+print(sum(t))
 
